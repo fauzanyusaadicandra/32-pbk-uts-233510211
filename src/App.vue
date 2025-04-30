eb1e047a7d8d8dfb8c8bbe9e90d90816c479d243
fauzanyusaadicandra <fauzanyuhuw@gmail.com>
2025-04-30 15:06:21 +0700

    Dapat melihat kegiatan yang sudah ditambahkan di aplikasi

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h1>Kegiatan Ku</h1>
+
+    <!-- Form untuk menambah kegiatan -->
+    <input v-model="newActivity" type="text" placeholder="Masukkan kegiatan baru" />
+
+    <!-- Menampilkan daftar kegiatan yang sudah ditambahkan -->
+    <ul>
+      <li v-for="(activity, index) in activities" :key="index">
+        {{ activity }}
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+export default {
+  data() {
+    return {
+      newActivity: '', // Untuk menampung input kegiatan baru
+      activities: []    // Array untuk menyimpan daftar kegiatan
+    };
+  },
+  methods: {
+    addActivity() {
+      if (this.newActivity.trim() !== '') {
+        this.activities.push(this.newActivity.trim()); // Menambahkan kegiatan ke array
+        this.newActivity = ''; // Reset input setelah ditambah
+      }
+    }
+  }
+};
+</script>
+
+<style>
+/* Styling dasar */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+input {
+  padding: 8px;
+  margin-right: 10px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+button {
+  padding: 8px 16px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #45a049;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 5px 0;
+  font-size: 18px;
 }
 </style>
